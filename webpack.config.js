2d53825361d79224c038b1e3074760667c9de219
your name here <nickbielak95@gmail.com>
2018-10-03 18:39:59 -0700

    More Heroku prep

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,28 @@
+
 const path = require('path');
-var webpack = require("webpack");
-
-var plugins = [];
-var devPlugins = []; 
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    "process.env": {
-      NODE_ENV: JSON.stringify("production")
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === "production" ? prodPlugins : devPlugins
-);
-
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/event_site.jsx",
+  entry: './frontend/event_site.jsx',
   output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".jsx", "*"]
+    extensions: ['.js', '.jsx', '*'],
   },
-  plugins: plugins,
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           query: {
-            presets: ["env", "react"]
-          }
-        }
-      }
-    ]
+            presets: ['env', 'react'],
+          },
+        },
+      },
+    ],
   },
-  devtool: "source-map"
-};
+  devtool: 'source-map',
