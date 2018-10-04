235fa6870991669bcc2bce4c840f7b08fd668148
your name here <nickbielak95@gmail.com>
2018-10-03 17:57:17 -0700

    Set up AWS production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  config.active_storage.service = :amazon_pro
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
