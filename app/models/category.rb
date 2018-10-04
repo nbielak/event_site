5a83bb8f5ba24db505671688772e24b9282e814a
your name here <nickbielak95@gmail.com>
2018-10-03 20:29:38 -0700

    Fix syntax error

--- app/models/category.rb
+++ app/models/category.rb
@@ -13,7 +13,7 @@ class Category < ApplicationRecord
     validates :name, :description, presence: true
     validates :name, uniqueness: true
 
-    has_many :event_categories,
+    has_many :event_categories
 
     has_many :events,
         through: :event_categories,
